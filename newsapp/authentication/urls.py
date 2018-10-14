77a399b0780130950837cb52c44123237b2361ea
zachang <zachangdawuda@gmail.com>
2018-10-14 22:23:50 +0100

    login form created

--- newsapp/authentication/urls.py
+++ newsapp/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RegistertView
+from .views import RegistertView, LoginView
 
 app_name = 'auth'
 urlpatterns = [
     path('register/', RegistertView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
--- newsapp/authentication/views.py
+++ newsapp/authentication/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 class RegistertView(FormView):
     template_name = 'register.html'
     form_class = RegisterForm
+    success_url = 'news/home.html'
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
     success_url = 'news/home.html'
